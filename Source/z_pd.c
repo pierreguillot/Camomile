8c05080fb40547706d3e793f2421003eb07c1fe3
pierreguillot <guillotpierre6@gmail.com>
2016-03-20 20:06:48 +0100

    update comment

--- Source/z_pd.c
+++ Source/z_pd.c
@@ -1,10 +1,12 @@
-//
-//  z_pd.c
-//  Camomile
-//
-//  Created by Guillot Pierre on 19/03/2016.
-//
-//
+/*
+// Copyright (c) 2015 Pierre Guillot.
+// For information on usage and redistribution, and for a DISCLAIMER OF ALL
+// WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+*/
+
+// This part of the code is greatly inspired by Pure Data and libPD, and sometimes
+// directly copied. None of the authors of Pure Data and libPD is responsible for these
+// experiments but you must be aware of their unintended contribution.
 
 #include "z_pd.h"
 #include "../ThirdParty/PureData/src/m_pd.h"
--- Source/z_pd.h
+++ Source/z_pd.h
@@ -4,6 +4,10 @@
 // WARRANTIES, see the file, "LICENSE.txt," in this distribution.
 */
 
+// This part of the code is greatly inspired by Pure Data and libPD, and sometimes
+// directly copied. None of the authors of Pure Data and libPD is responsible for these
+// experiments but you must be aware of their unintended contribution.
+
 #ifndef z_pd_h
 #define z_pd_h
 
