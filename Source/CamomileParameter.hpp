ad89fcd190191105a3319e1467cfcbb1a4e8f044
Pierre <guillotpierre6@gmail.com>
2016-06-20 18:08:16 +0200

    add namespace camo

--- Source/CamomileParameter.hpp
+++ Source/CamomileParameter.hpp
@@ -15,44 +15,45 @@
 //                                  CAMOMILE PARAMETER                                  //
 // ==================================================================================== //
 
-class CamomileParameter
+namespace camo
 {
-public:
-    CamomileParameter();
-    CamomileParameter(CamomileParameter const& other);
-    CamomileParameter(xpd::gui const& gui);
-    ~CamomileParameter();
+    class Parameter
+    {
+    public:
+        Parameter();
+        Parameter(Parameter const& other);
+        ~Parameter();
+        
+        Parameter& operator=(Parameter const& other);
+        Parameter& operator=(Parameter&& other);
+        
+        inline bool isValid() const noexcept {return m_valid;}
+        
+        inline float getValueNormalized() const noexcept {return m_value;}
+        inline float getValueNonNormalized() const noexcept {return (m_min < m_max) ? m_value * (m_max - m_min) + m_min : (1.f - m_value) * (m_min - m_max) + m_max;}
+        inline void setValueNonNormalized(float value) {m_value = getValueNormalized(value);}
+        void setValueNormalized(float value);
+        float getValueNormalized(float value);
+        inline float getDefaultValue() const noexcept {return 0.f;}
+        inline std::string getName() const {return m_name;}
+        inline std::string getLabel() const {return m_label;}
+        inline std::string getTextForValue() const {return std::to_string(getValueNonNormalized());}
+        inline float getValueForText(std::string const& text) const noexcept {return std::atof(text.c_str());}
+        inline bool isOrientationInverted() const noexcept {return false;}
+        inline int getNumberOfSteps() const noexcept {return m_nsteps != 0 ? m_nsteps : 0x7fffffff;}
+        inline xpd::tie const& getTie() const noexcept {return m_bname;}
+        
+    private:
+        bool        m_valid;
+        float       m_value;
+        float       m_min;
+        float       m_max;
+        std::string m_name;
+        std::string m_label;
+        int         m_nsteps;
+        xpd::tie    m_bname;
+    };
+}
 
-    CamomileParameter& operator=(CamomileParameter const& other);
-    CamomileParameter& operator=(CamomileParameter&& other);
-    
-    inline bool isValid() const noexcept {return m_valid;}
-    
-    inline float getValueNormalized() const noexcept {return m_value;}
-    inline float getValueNonNormalized() const noexcept {
-        return (m_min < m_max) ? m_value * (m_max - m_min) + m_min : (1.f - m_value) * (m_min - m_max) + m_max;}
-    void setValueNormalized(float value);
-    inline void setValueNonNormalized(float value) {m_value = getValueNormalized(value);}
-    float getValueNormalized(float value);
-    inline float getDefaultValue() const noexcept {return 0.f;}
-    
-    inline std::string getName() const {return m_name;}
-    inline std::string getLabel() const {return m_label;}
-    inline std::string getTextForValue() const {return std::to_string(getValueNonNormalized());}
-    inline float getValueForText(std::string const& text) const noexcept {return std::atof(text.c_str());}
-    inline bool isOrientationInverted() const noexcept {return false;}
-    inline int getNumberOfSteps() const noexcept {return m_nsteps != 0 ? m_nsteps : 0x7fffffff;}
-    inline xpd::tie const& getTie() const noexcept {return m_bname;}
-    
-private:
-    bool        m_valid;
-    float       m_value;
-    float       m_min;
-    float       m_max;
-    std::string m_name;
-    std::string m_label;
-    int         m_nsteps;
-    xpd::tie    m_bname;
-};
 
 #endif
