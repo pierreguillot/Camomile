04839ee311b1d2793e29a371ea08d9865ce8c4eb
pierreguillot <guillotpierre6@gmail.com>
2017-11-21 18:48:30 +0100

    update parser

--- Source/PluginParser.cpp
+++ Source/PluginParser.cpp
@@ -6,6 +6,42 @@
 
 #include "PluginParser.h"
 
+void CamomileParser::perform(juce::File const& file)
+{
+    var main;
+    FileInputStream stream(file);
+    if(stream.openedOk())
+    {
+        String line = stream.readNextLine();
+        while(line.isNotEmpty())
+        {
+            main.append(perform(line));
+            line = stream.readNextLine();
+        }
+    }
+}
 
-
-
+var CamomileParser::perform(String const& line)
+{
+    
+    DynamicObject obj;
+    Array<var> array;
+    int start = 0, next = line.indexOfChar(' ');
+    String const name = (next == -1)  ? line.substring(0) : line.substring(0, next);
+    while(next != -1)
+    {
+        start = next+1;
+        next = line.indexOfChar(start+1, ' ');
+        StringRef const word = (next == -1)  ? line.substring(0) : line.substring(0, next);
+        if(word.startsWithChar('-'))
+        {
+            array.add(var());
+        }
+        else
+        {
+            JSON::parse(word);
+        }
+    }
+    
+    return var();
+}
--- Source/PluginParser.h
+++ Source/PluginParser.h
@@ -16,10 +16,9 @@
 class CamomileParser
 {
 public:
-    static StringArray parseList(const std::vector<pd::Atom>& list, const String& name);
-    static String parseString(const std::vector<pd::Atom>& list, const String& name);
-    static float parseFloat(const std::vector<pd::Atom>& list, const String& name, float const def);
-    static int parseInt(const std::vector<pd::Atom>& list, const String& name, int const def);
-    static bool parseBool(const std::vector<pd::Atom>& list, const String& name, bool const def);
+    static void perform(juce::File const& file);
+    
+private:
+    static var perform(String const& line);
 };
 
