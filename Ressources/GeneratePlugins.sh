e181298e1adeefd304b13838c1e6c62e9d7f0dcb
pierreguillot <guillotpierre6@gmail.com>
2017-11-26 17:38:39 +0100

    change folder for patches

---
+++ Ressources/GeneratePlugins.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo Generates the default plugins
+VstPath = /Utilisateurs/Pierre/Bibliothèque/Audio/Plug-Ins/VST/
+Vst3Path = /Utilisateurs/Pierre/Bibliothèque/Audio/Plug-Ins/VST3/
+cp /Utilisateurs/Pierre/Bibliothèque/Audio/Plug-Ins/VST/Camomile.vst
