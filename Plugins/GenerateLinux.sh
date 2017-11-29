355ee03b1e108aab5048e871c97588c1068fe61d
pierreguillot <guillotpierre6@gmail.com>
2017-11-29 22:34:17 +0100

    update generate on OsX and add for Linux

--- Plugins/GenerateLinux.sh
+++ Plugins/GenerateLinux.sh
@@ -1,36 +1,11 @@
 #!/bin/bash
+echo Camomile Generates the new plugins
 
 CamomileInst=Camomile
 CamomileFx=CamomileFx
 
-VstExtension=vst
-Vst3Extension=vst3
-AuExtension=component
-
-VstPath=$HOME/Library/Audio/Plug-Ins/VST
-Vst3Path=$HOME/Library/Audio/Plug-Ins/VST3
-AuPath=$HOME/Library/Audio/Plug-Ins/Components
-
+VstExtension=so
 ThisPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-install_plugin() {
-    if [ -d $VstPath/$PatchName ]; then
-        rm -rf $VstPath/$PatchName
-    fi
-    cp -rf $ThisPath/Builds/$PatchName/ $VstPath/$PatchName
-    rm -rf $ThisPath/Builds/$PatchName
-}
-
-install_all_plugins() {
-    echo -n "install "
-    for Patch in $ThisPath/Builds/*
-    do
-      PatchName=${Patch##*/}
-      echo -n $PatchName " "
-      install_plugin
-    done
-    echo " "
-}
 
 generate_plugin() {
     if [ -d $ThisPath/Camomile/$CamomileName.$1 ]; then
@@ -38,7 +13,7 @@ generate_plugin() {
             rm -rf $ThisPath/Builds/$PatchName.$1
         fi
         cp -rf $ThisPath/Camomile/$CamomileName.$1/ $ThisPath/Builds/$PatchName.$1
-        cp -rf $PatchesPath/$PatchName/ $ThisPath/Builds/$PatchName.$1/Contents/Resources
+        cp -rf $PatchesPath/$PatchName/ $ThisPath/Builds/$PatchName
         echo -n $1 " "
     fi
 }
@@ -56,8 +31,6 @@ generate_plugins_in_folder() {
           echo -n $PatchName
           echo -n " ($1):"
           generate_plugin $VstExtension
-          generate_plugin $Vst3Extension
-          generate_plugin $AuExtension
           echo ""
       fi
     done
@@ -67,12 +40,7 @@ if [ ! -d $ThisPath/Builds ]; then
     mkdir $ThisPath/Builds
 fi
 
-echo Camomile Generates the new plugins from $ThisPath
 PatchesPath=$ThisPath/Effects
 generate_plugins_in_folder Effect
 PatchesPath=$ThisPath/Instruments
 generate_plugins_in_folder Instrument
-
-if [ "$1" == "install" ]; then
-    install_all_plugins
-fi
