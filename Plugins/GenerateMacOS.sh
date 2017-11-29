fe7ef3faadc21221c804246cb7ffe56d35e947d8
pierreguillot <guillotpierre6@gmail.com>
2017-11-29 17:03:52 +0100

    change installation directory to plugins

--- Plugins/GenerateMacOS.sh
+++ Plugins/GenerateMacOS.sh
@@ -32,29 +32,29 @@ do
       echo -n $PatchName
       echo -n " (Effect):"
       CamomileName=CamomileFx
-      if [ -d $VstPath/$CamomileName.$VstExtension ]; then
+      if [ -d $ThisPath/$CamomileName.$VstExtension ]; then
           if [ -d $VstPath/$PatchName.$VstExtension ]; then
               rm -rf $VstPath/$PatchName.$VstExtension
           fi
-          cp -rf $VstPath/$CamomileName.$VstExtension $VstPath/$PatchName.$VstExtension
+          cp -rf $ThisPath/$CamomileName.$VstExtension $VstPath/$PatchName.$VstExtension
           cp -rf $PatchesPath/$PatchName/ $VstPath/$PatchName.$VstExtension/Contents/Resources
           echo -n VST " "
       fi
 
-      if [ -d $Vst3Path/$CamomileName.$Vst3Extension ]; then
+      if [ -d $ThisPath/$CamomileName.$Vst3Extension ]; then
           if [ -d $Vst3Path/$PatchName.$Vst3Extension ]; then
               rm -rf $Vst3Path/$PatchName.$Vst3Extension
           fi
-          cp -rf $Vst3Path/$CamomileName.$Vst3Extension $Vst3Path/$PatchName.$Vst3Extension
+          cp -rf $ThisPath/$CamomileName.$Vst3Extension $Vst3Path/$PatchName.$Vst3Extension
           cp -rf $PatchesPath/$PatchName/ $Vst3Path/$PatchName.$Vst3Extension/Contents/Resources
           echo -n VST3 " "
       fi
 
-      if [ -d $AuPath/$CamomileName.$AuExtension ]; then
+      if [ -d $ThisPath/$CamomileName.$AuExtension ]; then
           if [ -d $AuPath/$PatchName.$AuExtension ]; then
               rm -r $AuPath/$PatchName.$AuExtension
           fi
-          cp -r $AuPath/$CamomileName.$AuExtension $AuPath/$PatchName.$AuExtension
+          cp -r $ThisPath/$CamomileName.$AuExtension $AuPath/$PatchName.$AuExtension
           cp -r $PatchesPath/$PatchName/ $AuPath/$PatchName.$AuExtension/Contents/Resources
           echo -n AudioUnit " "
       fi
@@ -70,29 +70,29 @@ do
       echo -n $PatchName
       echo -n " (Instrument):"
       CamomileName=Camomile
-      if [ -d $VstPath/$CamomileName.$VstExtension ]; then
+      if [ -d $ThisPath/$CamomileName.$VstExtension ]; then
           if [ -d $VstPath/$PatchName.$VstExtension ]; then
               rm -rf $VstPath/$PatchName.$VstExtension
           fi
-          cp -rf $VstPath/$CamomileName.$VstExtension $VstPath/$PatchName.$VstExtension
+          cp -rf $ThisPath/$CamomileName.$VstExtension $VstPath/$PatchName.$VstExtension
           cp -rf $PatchesPath/$PatchName/ $VstPath/$PatchName.$VstExtension/Contents/Resources
           echo -n VST " "
       fi
 
-      if [ -d $Vst3Path/$CamomileName.$Vst3Extension ]; then
+      if [ -d $ThisPath/$CamomileName.$Vst3Extension ]; then
           if [ -d $Vst3Path/$PatchName.$Vst3Extension ]; then
               rm -rf $Vst3Path/$PatchName.$Vst3Extension
           fi
-          cp -rf $Vst3Path/$CamomileName.$Vst3Extension $Vst3Path/$PatchName.$Vst3Extension
+          cp -rf $ThisPath/$CamomileName.$Vst3Extension $Vst3Path/$PatchName.$Vst3Extension
           cp -rf $PatchesPath/$PatchName/ $Vst3Path/$PatchName.$Vst3Extension/Contents/Resources
           echo -n VST3 " "
       fi
 
-      if [ -d $AuPath/$CamomileName.$AuExtension ]; then
+      if [ -d $ThisPath/$CamomileName.$AuExtension ]; then
           if [ -d $AuPath/$PatchName.$AuExtension ]; then
               rm -r $AuPath/$PatchName.$AuExtension
           fi
-          cp -r $AuPath/$CamomileName.$AuExtension $AuPath/$PatchName.$AuExtension
+          cp -r $ThisPath/$CamomileName.$AuExtension $AuPath/$PatchName.$AuExtension
           cp -r $PatchesPath/$PatchName/ $AuPath/$PatchName.$AuExtension/Contents/Resources
           echo -n AudioUnit " "
       fi
