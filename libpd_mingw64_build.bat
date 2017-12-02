c322ce98df7ad6e7c57a1a9ac20e9fc11ffc6f1a
pierreguillot <guillotpierre6@gmail.com>
2017-12-02 17:37:59 +0100

    fix typo

--- libpd_mingw64_build.bat
+++ libpd_mingw64_build.bat
@@ -1,5 +1,5 @@
 SET MSYS=C:\msys64
 SET MINGW=%MSYS%\mingw64
 SET PATH=%MINGW%\bin;%MSYS%\usr\bin
-make  -C libpdclobber
+make -C libpd clobber
 make -C libpd libpd ADDITIONAL_CFLAGS='-DPD_LONGINTTYPE="long long" MULTI=true'
