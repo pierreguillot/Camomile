ca7b07379837783a8edb6752aa5ad26e1bdc9c47
pierreguillot <guillotpierre6@gmail.com>
2017-12-02 14:22:28 +0100

    21

--- libpd_mingw64_build.bat
+++ libpd_mingw64_build.bat
@@ -2,4 +2,4 @@ SET MSYS=C:\msys64
 SET MINGW=%MSYS%\mingw64
 SET PATH=%MINGW%\bin;%MSYS%\usr\bin
 make clobber
-make -C libpd csharplib ADDITIONAL_CFLAGS='-DPD_LONGINTTYPE="long long" -DPDINSTANCE=1 -DPDTHREADS=1'
+make -C libpd libpd ADDITIONAL_CFLAGS='-DPD_LONGINTTYPE="long long" -DPDINSTANCE=1 -DPDTHREADS=1'
