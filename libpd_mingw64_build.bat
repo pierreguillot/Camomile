e8a8e46f224837f9729f270c21e7cc0bd413949c
pierreguillot <guillotpierre6@gmail.com>
2017-12-02 17:43:04 +0100

    fix typo sniff

--- libpd_mingw64_build.bat
+++ libpd_mingw64_build.bat
@@ -2,4 +2,4 @@ SET MSYS=C:\msys64
 SET MINGW=%MSYS%\mingw64
 SET PATH=%MINGW%\bin;%MSYS%\usr\bin
 make -C libpd clobber
-make -C libpd libpd ADDITIONAL_CFLAGS='-DPD_LONGINTTYPE="long long" MULTI=true'
+make -C libpd libpd MULTI=true ADDITIONAL_CFLAGS='-DPD_LONGINTTYPE="long long"'
