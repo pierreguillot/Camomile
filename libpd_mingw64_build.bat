0003e433b4f670234710de135a430caf79dca3e7
pierreguillot <guillotpierre6@gmail.com>
2018-01-05 11:53:21 +0100

    define link options (-static and -static-libgcc) in mingw batch script

--- libpd_mingw64_build.bat
+++ libpd_mingw64_build.bat
@@ -2,4 +2,4 @@ SET MSYS=C:\msys64
 SET MINGW=%MSYS%\mingw64
 SET PATH=%MINGW%\bin;%MSYS%\usr\bin
 make -C libpd clobber
-make -C libpd libpd MULTI=true ADDITIONAL_CFLAGS='-DPD_LONGINTTYPE="long long"'
+make -C libpd libpd MULTI=true LDFLAGS="$(MINGW_LDFLAGS) -Wl,--output-def=libs/libpd.def -static -static-libgcc -Wl,--out-implib=libs/libpd.lib" ADDITIONAL_CFLAGS='-DPD_LONGINTTYPE="long long"'
