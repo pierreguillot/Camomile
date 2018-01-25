d1e1d285304536c68d541bda2a5455e8fe7f80ce
pierreguillot <guillotpierre6@gmail.com>
2018-01-25 12:30:31 +0100

    Add plugin information for future use

---
+++ Source/PluginInformation.cpp
@@ -0,0 +1,26 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginInformation.h"
+#include "Gui/Gui.hpp"
+
+CamomileInformationDialog::CamomileInformationDialog()
+{
+    ;
+}
+
+CamomileInformationDialog::~CamomileInformationDialog()
+{
+    ;
+}
+
+
+
+
