97c32feacaa3c76323788f38cc627beb422fc9e0
pierreguillot <guillotpierre6@gmail.com>
2018-02-10 16:31:26 +0100

    test

--- Instrument/JuceLibraryCode/AppConfig.h
+++ Instrument/JuceLibraryCode/AppConfig.h
@@ -29,7 +29,6 @@
    under the GPL v3 license.
 
    End User License Agreement: www.juce.com/juce-5-licence
-
   ==============================================================================
 */
 
@@ -42,6 +41,7 @@
 #ifndef JUCE_REPORT_APP_USAGE
  #define JUCE_REPORT_APP_USAGE 1
 #endif
+
 
 // END SECTION A
 
@@ -67,7 +67,7 @@
 // juce_audio_devices flags:
 
 #ifndef    JUCE_ASIO
- //#define JUCE_ASIO 0
+ //#define JUCE_ASIO 1
 #endif
 
 #ifndef    JUCE_WASAPI
@@ -75,7 +75,7 @@
 #endif
 
 #ifndef    JUCE_WASAPI_EXCLUSIVE
- //#define JUCE_WASAPI_EXCLUSIVE 0
+ //#define JUCE_WASAPI_EXCLUSIVE 1
 #endif
 
 #ifndef    JUCE_DIRECTSOUND
@@ -87,19 +87,15 @@
 #endif
 
 #ifndef    JUCE_JACK
- //#define JUCE_JACK 0
+ //#define JUCE_JACK 1
 #endif
 
 #ifndef    JUCE_USE_ANDROID_OPENSLES
- //#define JUCE_USE_ANDROID_OPENSLES 0
+ //#define JUCE_USE_ANDROID_OPENSLES 1
 #endif
 
 #ifndef    JUCE_USE_WINRT_MIDI
- //#define JUCE_USE_WINRT_MIDI 0
-#endif
-
-#ifndef    JUCE_DISABLE_AUDIO_MIXING_WITH_OTHER_APPS
- //#define JUCE_DISABLE_AUDIO_MIXING_WITH_OTHER_APPS 0
+ //#define JUCE_USE_WINRT_MIDI 1
 #endif
 
 //==============================================================================
@@ -114,11 +110,11 @@
 #endif
 
 #ifndef    JUCE_USE_MP3AUDIOFORMAT
- //#define JUCE_USE_MP3AUDIOFORMAT 0
+ //#define JUCE_USE_MP3AUDIOFORMAT 1
 #endif
 
 #ifndef    JUCE_USE_LAME_AUDIO_FORMAT
- //#define JUCE_USE_LAME_AUDIO_FORMAT 0
+ //#define JUCE_USE_LAME_AUDIO_FORMAT 1
 #endif
 
 #ifndef    JUCE_USE_WINDOWS_MEDIA_FORMAT
@@ -129,56 +125,52 @@
 // juce_audio_plugin_client flags:
 
 #ifndef    JUCE_FORCE_USE_LEGACY_PARAM_IDS
- //#define JUCE_FORCE_USE_LEGACY_PARAM_IDS 0
+ //#define JUCE_FORCE_USE_LEGACY_PARAM_IDS 1
 #endif
 
 #ifndef    JUCE_FORCE_LEGACY_PARAMETER_AUTOMATION_TYPE
- //#define JUCE_FORCE_LEGACY_PARAMETER_AUTOMATION_TYPE 0
+ //#define JUCE_FORCE_LEGACY_PARAMETER_AUTOMATION_TYPE 1
 #endif
 
 #ifndef    JUCE_USE_STUDIO_ONE_COMPATIBLE_PARAMETERS
  //#define JUCE_USE_STUDIO_ONE_COMPATIBLE_PARAMETERS 1
 #endif
 
-#ifndef    JUCE_STANDALONE_FILTER_WINDOW_USE_KIOSK_MODE
- //#define JUCE_STANDALONE_FILTER_WINDOW_USE_KIOSK_MODE 0
-#endif
-
 //==============================================================================
 // juce_audio_processors flags:
 
 #ifndef    JUCE_PLUGINHOST_VST
- //#define JUCE_PLUGINHOST_VST 0
+ //#define JUCE_PLUGINHOST_VST 1
 #endif
 
 #ifndef    JUCE_PLUGINHOST_VST3
- //#define JUCE_PLUGINHOST_VST3 0
+ //#define JUCE_PLUGINHOST_VST3 1
 #endif
 
 #ifndef    JUCE_PLUGINHOST_AU
- //#define JUCE_PLUGINHOST_AU 0
+ //#define JUCE_PLUGINHOST_AU 1
 #endif
 
 //==============================================================================
 // juce_audio_utils flags:
 
 #ifndef    JUCE_USE_CDREADER
- //#define JUCE_USE_CDREADER 0
+ //#define JUCE_USE_CDREADER 1
 #endif
 
 #ifndef    JUCE_USE_CDBURNER
- //#define JUCE_USE_CDBURNER 0
+ //#define JUCE_USE_CDBURNER 1
 #endif
 
 //==============================================================================
 // juce_core flags:
 
 #ifndef    JUCE_FORCE_DEBUG
- //#define JUCE_FORCE_DEBUG 0
+ //#define JUCE_FORCE_DEBUG 1
 #endif
 
 #ifndef    JUCE_LOG_ASSERTIONS
- //#define JUCE_LOG_ASSERTIONS 0
+ //#define JUCE_LOG_ASSERTIONS 1
 #endif
 
 #ifndef    JUCE_CHECK_MEMORY_LEAKS
@@ -186,7 +178,7 @@
 #endif
 
 #ifndef    JUCE_DONT_AUTOLINK_TO_WIN32_LIBRARIES
- //#define JUCE_DONT_AUTOLINK_TO_WIN32_LIBRARIES 0
+ //#define JUCE_DONT_AUTOLINK_TO_WIN32_LIBRARIES 1
 #endif
 
 #ifndef    JUCE_INCLUDE_ZLIB_CODE
@@ -194,7 +186,7 @@
 #endif
 
 #ifndef    JUCE_USE_CURL
- //#define JUCE_USE_CURL 0
+ //#define JUCE_USE_CURL 1
 #endif
 
 #ifndef    JUCE_CATCH_UNHANDLED_EXCEPTIONS
@@ -209,7 +201,7 @@
 // juce_events flags:
 
 #ifndef    JUCE_EXECUTE_APP_SUSPEND_ON_IOS_BACKGROUND_TASK
- //#define JUCE_EXECUTE_APP_SUSPEND_ON_IOS_BACKGROUND_TASK 0
+ //#define JUCE_EXECUTE_APP_SUSPEND_ON_IOS_BACKGROUND_TASK 1
 #endif
 
 //==============================================================================
@@ -227,15 +219,7 @@
 // juce_gui_basics flags:
 
 #ifndef    JUCE_ENABLE_REPAINT_DEBUGGING
- //#define JUCE_ENABLE_REPAINT_DEBUGGING 0
-#endif
-
-#ifndef    JUCE_USE_XRANDR
- //#define JUCE_USE_XRANDR 1
-#endif
-
-#ifndef    JUCE_USE_XINERAMA
- //#define JUCE_USE_XINERAMA 1
+ //#define JUCE_ENABLE_REPAINT_DEBUGGING 1
 #endif
 
 #ifndef    JUCE_USE_XSHM
@@ -243,7 +227,7 @@
 #endif
 
 #ifndef    JUCE_USE_XRENDER
- //#define JUCE_USE_XRENDER 0
+ //#define JUCE_USE_XRENDER 1
 #endif
 
 #ifndef    JUCE_USE_XCURSOR
@@ -258,7 +242,7 @@
 #endif
 
 #ifndef    JUCE_ENABLE_LIVE_CONSTANT_EDITOR
- //#define JUCE_ENABLE_LIVE_CONSTANT_EDITOR 0
+ //#define JUCE_ENABLE_LIVE_CONSTANT_EDITOR 1
 #endif
 //==============================================================================
 #ifndef    JUCE_STANDALONE_APPLICATION
