706e0aea8af145b9ab2466141d577dc37d7cb0bd
pierreguillot <guillotpierre6@gmail.com>
2018-02-15 15:57:17 +0100

    fix Linux path for cmake

--- CamomileMake.sh
+++ CamomileMake.sh
@@ -2,11 +2,12 @@
 
 clear
 echo "Generate libpd"
-mkdir LibPd/builds/unixmakefile/Release
-cd LibPd/builds/unixmakefile/Release
+mkdir Dependencies/LibPd/builds/unixmakefile/
+mkdir Dependencies/LibPd/builds/unixmakefile/Release
+cd Dependencies/LibPd/builds/unixmakefile/Release
 cmake -DCMAKE_BUILD_TYPE=Release ../../..
 make libpdstatic
-cd ../../../..
+cd ../../../../..
 echo "Generate Camomile Instrument"
 make -C Instrument/Builds/LinuxMakefile TARGET_ARCH=-m64 CONFIG=Release
 echo "Generate Camomile Effect"
