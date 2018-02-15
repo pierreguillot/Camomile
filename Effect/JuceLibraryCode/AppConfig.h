f87253b60b0d48ff25d89a7c22f5c4d6a67eca1f
pierreguillot <guillotpierre6@gmail.com>
2018-02-15 11:41:13 +0100

    Fix the AppConfig definitions

--- Effect/JuceLibraryCode/AppConfig.h
+++ Effect/JuceLibraryCode/AppConfig.h
@@ -16,7 +16,7 @@
 //==============================================================================
 // [BEGIN_USER_CODE_SECTION]
 
-// (You can add your own code in this section, and the Projucer will not overwrite it)
+#include "../../Source/PluginConfig.h"
 
 // [END_USER_CODE_SECTION]
 
---
+++ Source/PluginConfig.h
@@ -0,0 +1,14 @@
+/*
+ // Copyright (c) 2015-2018 Pierre Guillot.
+ // For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ // WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+*/
+
+#pragma once
+
+#include "PluginEnvironment.h"
+#define JUCE_DISPLAY_SPLASH_SCREEN 0
+#define JUCE_REPORT_APP_USAGE 0
+#define JucePlugin_Name CamomileEnvironment::getPluginNameUTF8()
+#define JucePlugin_PluginCode CamomileEnvironment::getPluginCode()
+#define JucePlugin_Desc CamomileEnvironment::getPluginDescriptionUTF8()
