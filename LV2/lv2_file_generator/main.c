617bbdb4f025bf66f2c5e89200188aba6cd8762c
Pierre <guillotpierre6@gmail.com>
2018-07-03 18:49:29 +0200

    update approach

--- LV2/lv2_file_generator/main.c
+++ LV2/lv2_file_generator/main.c
@@ -12,51 +12,32 @@
 #include <string.h>
 
 #define MAX_STRING_LENGTH 1000
+
 #ifdef __APPLE__
 #define LIB_EXT "dylib"
+#elif _WIN32
+#define LIB_EXT "dll"
 #else
 #define LIB_EXT "so"
 #endif
+
 typedef void *(t_generate_file)(const char*);
 
 int main(int argc, const char * argv[]) {
-    
-    char* name;
-    char path[MAX_STRING_LENGTH];
-    char fullpath[MAX_STRING_LENGTH];
-    char realname[MAX_STRING_LENGTH];
-    size_t namesize;
-    if(argc < 2)
+    if(argc < 3)
     {
-        printf("Please provide the path to the LV2 plugin folder\n");
+        printf("Please provide the path to the LV2 plugin and the name of the plugin\n");
         return 1;
     }
-    
-    snprintf(path, MAX_STRING_LENGTH, "%s", argv[1]);
-    name = basename(path);
-    if(!name)
-    {
-        printf("Please provide the path to the LV2 plugin folder\n");
-        return 1;
-    }
-    
-    namesize = strnlen(name, 1000);
-    if(namesize < 4)
-    {
-        printf("The LV2 plugin folder is invalid\n");
-        return 1;
-    }
-    memcpy(realname, name, namesize-4);
-    snprintf(fullpath, 1000, "%s/%s.%s", argv[1], realname, LIB_EXT);
-    printf("looking for %s\n", fullpath);
-    void* handle = dlopen(fullpath, RTLD_LAZY);
+    printf("looking for %s in %s\n", argv[2], argv[1]);
+    void* handle = dlopen(argv[1], RTLD_LAZY);
     if(handle)
     {
         char* error = NULL;
         t_generate_file* method = (t_generate_file *)dlsym(handle, "lv2_generate_ttl");
         if(method)
         {
-            (*method)(realname);
+            (*method)(argv[2]);
         }
         else if((error = dlerror()) != NULL)
         {
@@ -66,7 +47,7 @@ int main(int argc, const char * argv[]) {
     }
     else
     {
-        
+
     }
     return 0;
 }
