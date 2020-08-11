34b287097b9276e0264a9c31d6546f425bbcf9ce
Pierre Guillot <pierre.guillot@ircam.fr>
2020-08-11 21:14:02 +0200

    update juce (6.0.1)

--- Effect/JuceLibraryCode/AppConfig.h
+++ Effect/JuceLibraryCode/AppConfig.h
@@ -25,12 +25,12 @@
 /*
   ==============================================================================
 
-   In accordance with the terms of the JUCE 5 End-Use License Agreement, the
+   In accordance with the terms of the JUCE 6 End-Use License Agreement, the
    JUCE Code in SECTION A cannot be removed, changed or otherwise rendered
    ineffective unless you have a JUCE Indie or Pro license, or are using JUCE
    under the GPL v3 license.
 
-   End User License Agreement: www.juce.com/juce-5-licence
+   End User License Agreement: www.juce.com/juce-6-licence
 
   ==============================================================================
 */
@@ -38,14 +38,14 @@
 // BEGIN SECTION A
 
 #ifndef JUCE_DISPLAY_SPLASH_SCREEN
- #define JUCE_DISPLAY_SPLASH_SCREEN 1
+ #define JUCE_DISPLAY_SPLASH_SCREEN 0
 #endif
 
 // END SECTION A
 
 #define JUCE_USE_DARK_SPLASH_SCREEN 1
 
-#define JUCE_PROJUCER_VERSION 0x60000
+#define JUCE_PROJUCER_VERSION 0x60001
 
 //==============================================================================
 #define JUCE_MODULE_AVAILABLE_juce_audio_basics             1
--- LV2/JuceLibraryCode/AppConfig.h
+++ LV2/JuceLibraryCode/AppConfig.h
@@ -33,12 +33,12 @@
 /*
   ==============================================================================
 
-   In accordance with the terms of the JUCE 5 End-Use License Agreement, the
+   In accordance with the terms of the JUCE 6 End-Use License Agreement, the
    JUCE Code in SECTION A cannot be removed, changed or otherwise rendered
    ineffective unless you have a JUCE Indie or Pro license, or are using JUCE
    under the GPL v3 license.
 
-   End User License Agreement: www.juce.com/juce-5-licence
+   End User License Agreement: www.juce.com/juce-6-licence
 
   ==============================================================================
 */
@@ -53,7 +53,7 @@
 
 #define JUCE_USE_DARK_SPLASH_SCREEN 1
 
-#define JUCE_PROJUCER_VERSION 0x60000
+#define JUCE_PROJUCER_VERSION 0x60001
 
 //==============================================================================
 #define JUCE_MODULE_AVAILABLE_juce_audio_basics                 1
